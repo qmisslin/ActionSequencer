41f2527f75ec5c0d1a3f10356b8c4aae1e1f8657
Misslin Quentin <misslin.quentin@gmail.com>
2021-12-29 20:00:23 +0100

    Add render class

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,46 +1,53 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const scene = new THREE.Scene()
+class Render {
+    scene: THREE.Scene
+    camera: THREE.PerspectiveCamera
+    renderer: THREE.WebGLRenderer
+    controls: OrbitControls
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+    constructor() {
+        this.scene = new THREE.Scene()
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+        this.camera.position.z = 2
+        this.renderer = new THREE.WebGLRenderer()
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        document.body.appendChild(this.renderer.domElement)
+        window.addEventListener('resize', () => this.resize(), false)
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        this.createScene()
+    }
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+    createScene() {
+        const geometry = new THREE.BoxGeometry()
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00,
+            wireframe: true,
+        })
+        const cube = new THREE.Mesh(geometry, material)
+        this.scene.add(cube)
+    }
 
-const controls = new OrbitControls(camera, renderer.domElement)
+    resize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.draw()
+    }
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+    loop() {
+        this.controls.update()
+        this.draw()
+        requestAnimationFrame(() => this.loop())
+    }
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+    draw() {
+        console.log('draw')
+        this.renderer.render(this.scene, this.camera)
+    }
 }
 
-function animate() {
-    requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    controls.update()
-
-    render()
-}
-
-function render() {
-    renderer.render(scene, camera)
-}
-animate()
+const render = new Render()
+render.loop()
